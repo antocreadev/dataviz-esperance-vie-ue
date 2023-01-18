5d489a2105a276e06dd73553b5f22dd5e3fc597c
anthony <anthony@MacBook-Air-de-anthony.local>
2023-01-18 18:21:52 +0100

    mise à jour v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,21 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    width: {
+      "10vw": "10vw",
+      "50vw": "50vw",
+      "100vw": "100vw",
+      "120vw": "120vw",
+      "150vw": "150vw",
+    },
+    height: {
+      "10vh": "10vh",
+      "50vh": "50vh",
+      "100vh": "100vh",
+      "120vh": "120vh",
+      "150vh": "150vh",
+    },
+
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
