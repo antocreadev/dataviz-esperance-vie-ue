9ff414b4d72d9e56311ddb8d6b5b38c7918b88d6
anthony <anthony@MacBook-Air-de-anthony.local>
2023-02-09 00:13:27 +0100

    balise head

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 export default function Navbar() {
   return (
-    <nav className="navbar">
+    <nav className="navbar" style={{ backgroundColor: "white" }}>
       <Link href="./" className="btn btn-ghost normal-case text-xl">
         <span>DataViz - life span in the European Union</span>
       </Link>
